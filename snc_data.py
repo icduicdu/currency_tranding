d65edc50b279eb594c5fdfd14bebc90f3ff0b72a
Viacheslav Kotenko <icdu@yandex.com>
2016-10-25 20:16:57 +0300

    load data from csv

--- snc_data.py
+++ snc_data.py
@@ -1,61 +1,30 @@
-from itertools import permutations
+import csv
+from random import randint, random
 
 currency_codes = ['USD', 'CNY', 'RUB']
-
 currencies = ['EURO', 'USD', 'CNY', 'RUB']
-
-our_currency = 'EURO'
 our_money_amount = 1.0
 
 separator = ' -> '
+currency_rate = None
 
 
-def get_rates():
+def read_rates():
+    global currency_rate
     currency_rate = {}
-    currency_rate['USD/USD'] = 1.0
-    currency_rate['USD/EURO'] = 0.9
-    currency_rate['USD/CNY'] = 6.0
-    currency_rate['USD/RUB'] = 65.0
-    currency_rate['EURO/USD'] = 1.1
-    currency_rate['EURO/EURO'] = 1.0
-    currency_rate['EURO/CNY'] = 6.5
-    currency_rate['EURO/RUB'] = 70.0
-    currency_rate['CNY/USD'] = 0.17
-    currency_rate['CNY/EURO'] = 0.16
-    currency_rate['CNY/CNY'] = 1.0
-    currency_rate['CNY/RUB'] = 10.75
-    currency_rate['RUB/USD'] = 0.02
-    currency_rate['RUB/EURO'] = 0.01
-    currency_rate['RUB/CNY'] = 0.08
-    currency_rate['RUB/RUB'] = 1.0
-    return currency_rate
+    f = open('data.csv', 'rb')
+    reader = csv.reader(f)
+    idx = 0
+    for row in reader:
+        row_list = row[0].split()
+        for i in range(len(currencies)):
+            currency_rate[currencies[idx] + '/' + currencies[i]] = float(row_list[i])
+        idx += 1
+    f.close()
 
 
 def get_rate(c_from, c_to):
-    rates = get_rates()
-    return rates[c_from + '/' + c_to]
-
-
-def get_sequences():
-    _list = []
-    i = len(currency_codes) + 1
-    while i > 1:
-        i -= 1
-        res = list(permutations(currency_codes, i))
-        _list += res
-    return _list
-
-
-def print_non_risk_seq(li, k):
-    res = str(k) + ' ' + our_currency + separator
-    for s in li:
-        res += s + ' -> '
-    res += our_currency
-    print res
-
-
-def convert_seq(_list):
-    res = 1
-    for i in range(1, len(_list)):
-        res *= get_rate(_list[i-1], _list[i])
-    print res
+    global currency_rate
+    if currency_rate is None:
+        read_rates()
+    return currency_rate[c_from + '/' + c_to]
--- snc_dumb.py
+++ snc_dumb.py
@@ -1,16 +1,32 @@
+from itertools import permutations
+from snc_data import our_money_amount, currencies, separator, currency_codes, get_rate
 
-from snc_data import get_rates, get_sequences, our_money_amount, our_currency, print_non_risk_seq
 
-currency_rate = get_rates()
-sequences = get_sequences()
+def print_non_risk_seq(li, k):
+    res = str(k) + ' ' + currencies[0] + separator
+    for s in li:
+        res += s + ' -> '
+    res += currencies[0]
+    print res
 
-for seq in sequences:
-    current_money_amount = our_money_amount
-    current_currency = our_currency
-    current_list = list(seq)
-    for i in current_list:
-        current_money_amount = currency_rate[current_currency + '/' + i] * current_money_amount
-        current_currency = i
-    amount_in_our_currency = currency_rate[current_currency + '/' + our_currency] * current_money_amount
-    if amount_in_our_currency > 1.0:
-        print_non_risk_seq(current_list, amount_in_our_currency)
+
+def run_dumb():
+    sequences = []
+    i = len(currency_codes) + 1
+    while i > 1:
+        i -= 1
+        res = list(permutations(currency_codes, i))
+        sequences += res
+
+    for seq in sequences:
+        current_money_amount = our_money_amount
+        current_currency = currencies[0]
+        current_list = list(seq)
+        for i in current_list:
+            current_money_amount = get_rate(current_currency, i) * current_money_amount
+            current_currency = i
+        amount_in_our_currency = get_rate(current_currency, currencies[0]) * current_money_amount
+        if amount_in_our_currency > 1.0:
+            print_non_risk_seq(current_list, amount_in_our_currency)
+
+run_dumb()
